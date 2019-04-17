8bbfb97931fcbb2531c2effd8e584c7e0da282a4
visualstudio <visualstudio@122-PC16>
2019-04-17 10:04:45 -0400

    LongestWord

--- C0732156-CSD3354-A5-Part2/Program.cs
+++ C0732156-CSD3354-A5-Part2/Program.cs
@@ -10,6 +10,34 @@ namespace C0732156_CSD3354_A5_Part2
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("The Longest Word is : ");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+        }
+
+        public void MyFunctionA(string input)
+        {
+            string[] words = input.Split('');
+
+            int wordArrayLength = words.Length;
+            int[] wordsLenth = new int[wordArrayLength];
+            int x = 0;
+
+            foreach (var word in words)
+            {
+
+            }
+            string currentWord = words[0];
+            string nextWord;
+            string longestWord = currentWord;
+
+            for (int y = 0; y <words.Length - 1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+
+            }
         }
     }
 }
+        
